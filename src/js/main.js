b93bc7fd5f744dbe1c9dd977a939f01c2c5a05db
Pampaj <dawidkawinski@opoczta.pl>
2023-07-10 00:00:10 +0200

    Co-authored-by: McRaZick <mcrazick@mail.com> Co-authored-by: MaCiRiXxX <MaCiRiXxX@users.noreply.github.com>

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,66 @@
+/*
+
+myFunction() {
+
+}
+myFunction()
+
+// Expresia
+( function() {} )()
+
+(() => {
+
+})();
+
+//Arrow function
+const myFunction = () => {
+
+} 
+
+//annonymous function
+function(){
+
+}
+
+//annonymous arrow function
+() => {
+
+}
+
+
+Zobacz se co to recurrsion functions (reoccuring function)
+
+*/
+
+
+/*
+function onButtonClick() {
+    saveItemToItems(newItem());
+}
+
+btn_submit.addEventListener('click', onButtonClick);
+btn_submit.removeEventListener('click', onButtonClick);
+
+
+
+
+
+i to sie nazywa 'index'
+    i++ or ++i   to i += 1   lub i = i + 1
+    i-- or --i   to i -= 1   lub i = i - 1
+
+    * Big O diagram 
+*/
+
+
+
 let btn_submit = document.querySelector("#menu button[type='button']");
 
-btn_submit.addEventListener('click', newItem);
+btn_submit.addEventListener('click', () => {
+    saveItemToItems(newItem());
+});
+
+
 
 function newItem() {
     
@@ -12,24 +72,82 @@ function newItem() {
 
     // If no price nor title length, "early-return"
     // Jeżeli długość ceny lub tytułu jest pusta, to przerwij dalsze akcje
-    if (price_value.length === 0
-    ||  title_value.lenght === 0) return;
+    if (price_value.length <= 0
+    ||  title_value.lenght <= 0) return;
+
+    // ustawia id pustej listy na 0 zamiast undefined
+    let id = getItemWithBiggestIDFromItemsList();
+        id = (id === undefined) ? 0 : id;
+
+    // Construct new item
+    let item  = { id, title_value, price_value }
+
 
     console.table({
-        "title_length" : [title_value, title_value.length],
-        "price_length" : [price_value, price_value.length]
+        "id":[item.id, item.id.length],
+        "title":[item.title_value, item.title_value.length],
+        "value":[item.price_value, item.price_value.length]
+
     });
 
 
-    // Zapisz nowy item do localStorage 'items' listy
-    let items = JSON.parse(localStorage.getItem("items"));
-    if (!Array.isArray(items)) throw new Error('localStorage "items" is not a list!');
-        
+    return item;
+}
 
-    let item  = { title_value, price_value }
-        items.push(item);
+
+
+function getItemWithBiggestIDFromItemsList() {
+    
+    let list = getOrCreateEmptyItemsList();
+    if (!list.length) return; // early-return
+
+    let biggest_id = -1;
+ 
+    // Zapetl (iteruj po instancjach listy)
+    for (let i = 0; i < list.length; i++) {
+        let item = list[i];
+
+        
+        if (item.id > biggest_id) {
+            biggest_id = item.id;
+        }
+    }
+
+    return biggest_id;
+}
+
+
+
+function getOrCreateEmptyItemsList() {
+    let items = undefined;
+
+    try {
+        items = JSON.parse(localStorage.getItem("items") || "");
+    }
+    catch (error) {
+        items = [];
+        console.error(error);
+    }
+
+    return items;
+}
+
+
+
+function saveItemToItems(item) {
+    // item  = { title_value, price_value } from newItem();
+    console.log(item);
+    if (!item) return; // early-return if item is 'empty' or 'undefined' or 'false'
+
+
+    // undefined - null - false
+    localStorage.getItem("items");
+
+    let items = getOrCreateEmptyItemsList();
+
+    // Zapisz nowy item do localStorage 'items' listy
+    items.push(item);
 
     // Zapisz liste itemów jako text w formie JSON
-
-    localStorage.setItem("items",JSON.stringify(items)); 
+    localStorage.setItem("items", JSON.stringify(items)); 
 }
